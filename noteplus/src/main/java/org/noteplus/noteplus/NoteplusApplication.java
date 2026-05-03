67fb95c4fea40b512c3a98d84eea16e66551ce62
Kay van Huizen <0328120@student.rocvantwente.nl>
2026-05-03 22:30:40 +0200

    Initial commit project setup

---
+++ noteplus/src/main/java/org/noteplus/noteplus/NoteplusApplication.java
@@ -0,0 +1,13 @@
+package org.noteplus.noteplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteplusApplication.class, args);
+    }
+
+}
---
+++ noteplus/src/test/java/org/noteplus/noteplus/NoteplusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.noteplus.noteplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
