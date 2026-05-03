7c3f1472e253a185f40725cbab416036b0d53acf
Kay van Huizen <0328120@student.rocvantwente.nl>
2026-05-03 22:52:17 +0200

    added Category modal and enum types

---
+++ noteplus/src/main/java/org/noteplus/noteplus/entity/CategoryColor.java
@@ -0,0 +1,14 @@
+package org.noteplus.noteplus.entity;
+
+public enum CategoryColor {
+    PINK,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    TEAL,
+    BLUE,
+    PURPLE,
+    GRAY,
+    DEFAULT
+}
