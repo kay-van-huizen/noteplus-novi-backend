fa72b52a7ccd718057bf9f787da088a2262ab597
Kay van Huizen <0328120@student.rocvantwente.nl>
2026-05-10 10:08:20 +0200

    Added service implementation for changing password, added dto's and index.jte for in settings

--- noteplus/src/main/java/org/noteplus/noteplus/repository/UserRepository.java
+++ noteplus/src/main/java/org/noteplus/noteplus/repository/UserRepository.java
@@ -2,6 +2,8 @@ package org.noteplus.noteplus.repository;
 
 import org.noteplus.noteplus.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,5 +14,10 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     boolean existsByUsername(String username);
 
+    Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findByLongId(@Param("id") Long id);
 }
